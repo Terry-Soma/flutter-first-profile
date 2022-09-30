d204fb3f40ca0bcea103d00f560525eadf3b7684
Terry-Soma <jackhack116@yahoo.com>
2022-09-30 21:22:59 +0800

    magic 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 /* starting code */
@@ -18,65 +20,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/nothing.jpg"),
-                ),
-                const Text(
-                  "Body Guard",
-                  style: TextStyle(
-                      fontSize: 32.0,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico'),
-                ),
-                Text(
-                  "MOBILE DEVELOPER",
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.teal[100],
-                      fontFamily: 'Source Sans Pro',
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  width: 180,
-                  height: 24,
-                  child: Divider(color: Colors.teal[100]),
-                ),
-                Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.teal[900],
-                      ),
-                      title: Text(
-                        "+976) 91192825",
-                        style: TextStyle(color: Colors.teal[900], fontSize: 16),
-                      ),
-                    )),
-                Card(
-                  color: Colors.white,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email_outlined,
-                      color: Colors.teal[900],
-                    ),
-                    title: Text("jakleito70@gmail.com",
-                        style:
-                            TextStyle(color: Colors.teal[900], fontSize: 16)),
-                  ),
-                )
-              ],
-            ))));
+            backgroundColor: Colors.blue[600],
+            appBar: AppBar(
+              title: const Text("Ask me anything"),
+              backgroundColor: Colors.blue[900],
+            ),
+            body: const BallPage()));
+  }
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+  void clickHandler() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: clickHandler,
+          child: Image.asset("images/ball$ballNumber.png"),
+        ),
+      ),
+    );
   }
 }
