237436bcb291d8a532424447ec88d9e6357c360f
Terry-Soma <jackhack116@yahoo.com>
2022-10-11 00:19:03 +0800

    dart oop concepts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'quizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+QuizBrain quizBrain = QuizBrain();
 /* starting code */
 void main() {
-  runApp(const MyApp());
+  runApp(const Quizzler());
 }
 
 ///*************  Acknowledge *********************
@@ -11,72 +14,134 @@ void main() {
 //=>  ====    {}
 
 //*/
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/nothing.jpg"),
-                ),
-                const Text(
-                  "Body Guard",
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
+      if (quizBrain.isFinished()) {
+        //TODO Step 4 Part A - show an alert using rFlutter_alert,
+
+        //This is the code for the basic alert from the docs for rFlutter Alert:
+        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+
+        //Modified for our purposes:
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz., ',
+          // buttons: [
+          //   DialogButton(
+          //     child: Text(
+          //       "COOL",
+          //       style: TextStyle(color: Colors.white, fontSize: 20),
+          //     ),
+          //     onPressed: () => Navigator.pop(context),
+          //     width: 120,
+          //   )
+          // ],
+        ).show();
+
+        //TODO Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+      }
+
+      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(const Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(const Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                child: const Text(
+                  "True",
                   style: TextStyle(
-                      fontSize: 32.0,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico'),
+                      color: Color.fromARGB(255, 141, 223, 152),
+                      fontSize: 20.0),
                 ),
-                Text(
-                  "MOBILE DEVELOPER",
+                onPressed: () => checkAnswer(true)),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                child: const Text(
+                  "False",
                   style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.teal[100],
-                      fontFamily: 'Source Sans Pro',
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.w500),
+                      color: Color.fromARGB(255, 231, 0, 0), fontSize: 20.0),
                 ),
-                SizedBox(
-                  width: 180,
-                  height: 24,
-                  child: Divider(color: Colors.teal[100]),
-                ),
-                Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.teal[900],
-                      ),
-                      title: Text(
-                        "+976) 91192825",
-                        style: TextStyle(color: Colors.teal[900], fontSize: 16),
-                      ),
-                    )),
-                Card(
-                  color: Colors.white,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email_outlined,
-                      color: Colors.teal[900],
-                    ),
-                    title: Text("jakleito70@gmail.com",
-                        style:
-                            TextStyle(color: Colors.teal[900], fontSize: 16)),
-                  ),
-                )
-              ],
-            ))));
+                onPressed: () => checkAnswer(false)),
+          ),
+        ),
+        Row(children: scoreKeeper),
+      ],
+    );
   }
 }
