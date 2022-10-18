02e6bf9c3ab8feed271e3414ae1481d6122f0076
Terry-Soma <jackhack116@yahoo.com>
2022-10-18 23:23:32 +0800

    maybe i am very bad

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/story.dart';
+import 'package:flutter_application_1/story_brain.dart';
 
 /* starting code */
 void main() {
-  runApp(const MyApp());
+  runApp(const Destini());
 }
 
 ///*************  Acknowledge *********************
@@ -11,72 +13,126 @@ void main() {
 //=>  ====    {}
 
 //*/
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Destini extends StatelessWidget {
+  const Destini({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/nothing.jpg"),
-                ),
-                const Text(
-                  "Body Guard",
-                  style: TextStyle(
-                      fontSize: 32.0,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico'),
-                ),
-                Text(
-                  "MOBILE DEVELOPER",
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.teal[100],
-                      fontFamily: 'Source Sans Pro',
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  width: 180,
-                  height: 24,
-                  child: Divider(color: Colors.teal[100]),
-                ),
-                Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.teal[900],
-                      ),
-                      title: Text(
-                        "+976) 91192825",
-                        style: TextStyle(color: Colors.teal[900], fontSize: 16),
-                      ),
-                    )),
-                Card(
-                  color: Colors.white,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email_outlined,
-                      color: Colors.teal[900],
-                    ),
-                    title: Text("jakleito70@gmail.com",
-                        style:
-                            TextStyle(color: Colors.teal[900], fontSize: 16)),
-                  ),
-                )
-              ],
-            ))));
+      theme: ThemeData.dark(),
+      home: const StoryPage(),
+    );
   }
 }
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  StoryBrain strBrain = StoryBrain();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Center(
+                    child: Text(
+                      strBrain.getStory(),
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  child: SizedBox(
+                    width: 300,
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          strBrain.nextStory(1);
+                        });
+                      },
+                      child: Text(
+                        strBrain.getChoice1(),
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: Visibility(
+                  visible: strBrain.buttonShouldBeVisible(),
+                  child: SizedBox(
+                    width: 300,
+                    child: TextButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.blue)),
+                      onPressed: () {
+                        setState(() {
+                          strBrain.nextStory(2);
+                        });
+                      },
+                      child: Text(
+                        strBrain.getChoice2(),
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+//
+//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
+
+
+// //TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+
+
+
+             
+
+//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
